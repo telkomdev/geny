f11dcba9b31c437ec5363328ce585159a54cd413
Wuriyanto <wuriyanto48@yahoo.co.id>
2019-08-16 17:03:38 +0700

    add ascii colors

---
+++ colors.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const COLORS = {
+    reset : "\x1b[0m",
+    bright : "\x1b[1m",
+    dim : "\x1b[2m",
+    underscore : "\x1b[4m",
+    blink : "\x1b[5m",
+    reverse : "\x1b[7m",
+    hidden : "\x1b[8m",
+    black : "\x1b[30m",
+    red : "\x1b[31m",
+    green : "\x1b[32m",
+    yellow : "\x1b[33m",
+    blue : "\x1b[34m",
+    magenta : "\x1b[35m",
+    cyan : "\x1b[36m",
+    white : "\x1b[37m",
+    bBlack : "\x1b[40m",
+    bRed : "\x1b[41m",
+    bGreen : "\x1b[42m",
+    bYellow : "\x1b[43m",
+    bBlue : "\x1b[44m",
+    bMagenta : "\x1b[45m",
+    bCyan : "\x1b[46m",
+    bWhite : "\x1b[47m"
+};
+
+const giveColor = (text, color) => {
+    return `${COLORS[color]}${text}${COLORS.reset}`;
+};
+
+module.exports = giveColor;
